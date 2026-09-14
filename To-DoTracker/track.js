a79d511874ed43ade02802efd7e6b9a24be16879
John S Murmu <johnsilasmurmu@gmail.com>
2026-09-14 20:59:34 +0530

    removed unnecessary lines

--- To-DoTracker/track.js
+++ To-DoTracker/track.js
@@ -2,20 +2,15 @@ let addingTask = document.querySelector(".adding");
 let addButton = document.querySelector("button");
 let taskMsg = document.querySelector(".taskText");
 
-
-
-
-
 let count=0;
 addButton.addEventListener("click",(evt) => {
     evt.preventDefault();
-    let newDiv = document.createElement("div");
+    let newDiv = document.createElement("li");
     newDiv.classList.add("textBorderDesign");
     let newcheckbox = document.createElement("input");
     newcheckbox.type="checkbox";
     newcheckbox.classList.add("newcheckboxDesign");
     let deleteButton = document.createElement("button");
-    deleteButton.value="delete";
     deleteButton.classList.add("deleteButtonDesign");
     let trashIcon = document.createElement("i");
     trashIcon.classList.add("fa-solid", "fa-trash");
@@ -23,12 +18,13 @@ addButton.addEventListener("click",(evt) => {
     let container = document.createElement("div");
     container.classList.add("containerDesign");
     taskMsg.appendChild(container);
-    count++;
-    newDiv.innerText = `${count}. ${addingTask.value}`;
+    
+    newDiv.innerText = `${addingTask.value}`;
     container.appendChild(newDiv);
     container.appendChild(newcheckbox);
     container.appendChild(deleteButton);
     addingTask.value="Enter new tasks";
+    
     deleteButton.addEventListener("click", (evt) => {
         evt.preventDefault();
         newDiv.remove();
