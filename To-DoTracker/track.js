5ccfc392fb442a0cb55dfde4c05e97b7909c33f3
John S Murmu <johnsilasmurmu@gmail.com>
2026-09-15 20:59:43 +0530

    final: Improved the code

--- To-DoTracker/track.js
+++ To-DoTracker/track.js
@@ -2,34 +2,34 @@ let addingTask = document.querySelector(".adding");
 let addButton = document.querySelector("button");
 let taskMsg = document.querySelector(".taskText");
 
-let count=0;
 addButton.addEventListener("click",(evt) => {
     evt.preventDefault();
-    let newDiv = document.createElement("li");
+    if(addingTask.value.trim() ==="")return;
+
+    let newDiv = document.createElement("ol");
     newDiv.classList.add("textBorderDesign");
+
     let newcheckbox = document.createElement("input");
     newcheckbox.type="checkbox";
     newcheckbox.classList.add("newcheckboxDesign");
     let deleteButton = document.createElement("button");
     deleteButton.classList.add("deleteButtonDesign");
-    let trashIcon = document.createElement("i");
-    trashIcon.classList.add("fa-solid", "fa-trash");
-    deleteButton.appendChild(trashIcon);
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+
     let container = document.createElement("div");
     container.classList.add("containerDesign");
-    taskMsg.appendChild(container);
+    
     
     newDiv.innerText = `${addingTask.value}`;
     container.appendChild(newDiv);
     container.appendChild(newcheckbox);
     container.appendChild(deleteButton);
-    addingTask.value="Enter new tasks";
-    
+    taskMsg.appendChild(container);
+    addingTask.value = "";
+
     deleteButton.addEventListener("click", (evt) => {
         evt.preventDefault();
-        newDiv.remove();
-        newcheckbox.remove();
-        deleteButton.remove();
+        container.remove();
         count=0;
     });
 });
